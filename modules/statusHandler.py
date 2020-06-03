238589b1ff8f1e82e4d2b77662acf6fb5c40c231
mja00 <me@mja00.dev>
2020-06-03 15:10:31 -0400

    Finished statusHandler.py and created an API wrapper

--- modules/statusHandler.py
+++ modules/statusHandler.py
@@ -1,8 +1,9 @@
 from discord.ext import commands
 from colorama import init, Fore, Style, Back
-from discord import File
+from discord import File, Embed
 from discord.utils import get
 from datetime import datetime as dt
+from mcprostatus import Location, Node
 import discord, os, json, requests, random
 
 
@@ -11,12 +12,34 @@ class statusHandler(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def nodestatus(self, ctx, node: int = None):
+    async def nodestatus(self, ctx, node: str = None):
         if node is None:
             await ctx.channel.send("Please specify a node number.")
             return
-        await ctx.channel.send("Getting node status")
+        message = await ctx.channel.send("Getting node status")
+        try:
+            node = Node(node)
+        except KeyError:
+            await message.delete()
+            await ctx.channel.send("Node doesn't exist")
+        embed = self.createEmbed(node)
+        await message.delete()
+        await ctx.channel.send(embed=embed)
 
+    def createEmbed(self, node: Node):
+        embed = Embed(
+            title=node.node,
+            color=discord.Color.green() if node.online else discord.Color.red()
+        )
+
+        embed.timestamp = dt.utcnow()
+
+        embed.add_field(name="Location", value=node.location, inline=True)
+        embed.add_field(name="Online", value=node.online, inline=True)
+        embed.add_field(name="Network Issue", value=node.network_issue, inline=True)
+        embed.add_field(name="Message", value=node.message, inline=True)
+        embed.add_field(name="Last Hearbeat", value=node.last_heartbeat, inline=True)
+        return embed
 
 
 def setup(bot):
